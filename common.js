f5e0df95b968bfce7a63c77dbf94e34adc0b555e
IDigle1 <56547199+IDigle1@users.noreply.github.com>
2019-10-14 23:39:32 +0500

    Update common.js

--- common.js
+++ common.js
@@ -1,1 +1,6 @@
-console.log('it's work')
+const aboutMeheader = document.querySelector('.about-me');
+const text = document.querySelector('.about-me .text');
+
+aboutMeheader.addEventListener('click', function() {
+    text.classList.toggle('hidden');
+});
