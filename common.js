50bb088f10fbdae37028bb52641b68915ddf1a9a
IDigle1 <56547199+IDigle1@users.noreply.github.com>
2019-11-11 05:23:20 +0500

    Delete common.js

--- common.js
+++
@@ -1,6 +0,0 @@
-const aboutMeheader = document.querySelector('.about-me');
-const text = document.querySelector('.about-me .text');
-
-aboutMeheader.addEventListener('click', function() {
-    text.classList.toggle('hidden');
-});
