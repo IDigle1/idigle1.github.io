dc88a264c9fcf91efcd2bf449ba93da54229de46
idigle1 <falalei1995@gmail.com>
2019-12-02 18:13:35 +0500

    update index

--- work/js/common.js
+++ work/js/common.js
@@ -15,8 +15,8 @@ let ccloud = {
   startInterval: function(arr, interval) {
     let i = 0;
     setInterval(function() {
-        ccloud.change(arr[i]);
-        i++
+        ccloud.change(arr[i][0]);
+        i++;
         if(i >= arr.length) i = 0;
     },interval);
   }
