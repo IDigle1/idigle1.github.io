2fce6c1aa9b9621f98753ceb1a493900a7ccf53a
idigle1 <falalei1995@gmail.com>
2019-12-03 17:03:27 +0500

    update index

--- work/js/common.js
+++ work/js/common.js
@@ -21,3 +21,35 @@ let ccloud = {
     },interval);
   }
 }
+// var percentageComplete = parseInt($('svg#progress-bar').attr(
+//   'data-percent')) / 100;
+// var strokeDashOffsetValue = 100 - (percentageComplete * 100);
+// var progressBar = $("svg#progress-bar .js-progress-bar");
+// progressBar.css("stroke-dashoffset", strokeDashOffsetValue);
+// $('svg#progress-bar text').text(`+${percentageComplete * 100}%`);
+
+jQuery.fn.progressBar = function(percent) {
+  var currentText = this.find('text').text().trim();
+  
+  var current = parseInt( currentText.match(/[0-9]/g).join('') );
+  console.log(current);
+  var percentageComplete = percent / 100;
+  var strokeDashOffsetValue = 100 - (percentageComplete * 100);
+  var progressBar = this.find(" .js-progress-bar");
+  progressBar.css("stroke-dashoffset", strokeDashOffsetValue);
+
+  // function transition(ctx, from, to) {
+  //   let delay = from / 1000;
+  //   let int = from;
+  //   let interval = setInterval(function() {
+  //     if(from == to) clearInterval(interval);
+  //     if(from < to) int++;
+  //     if(from > to) int--;
+      
+  //     ctx.find('text').text(`+${to}%`);
+  //   }, delay);
+  // }
+
+  // transition(this, current, percentageComplete * 100);
+  this.find('text').text(`+${percentageComplete * 100}%`);
+};
